6664f8b5bc89700d91addc6bcad14a46e947f46a
Sergey Lebedev <sergejlebedev31838@gmail.com>
2024-07-15 00:57:45 +0700

    Update urls.py

--- advertisements/advertisements/urls.py
+++ advertisements/advertisements/urls.py
@@ -28,12 +28,11 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
     )
 else:
     settings.DEBUG = False
