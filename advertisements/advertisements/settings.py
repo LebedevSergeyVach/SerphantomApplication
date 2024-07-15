22f64e451bd729b503f2c302bbe2489ec30db93b
Sergey Lebedev <sergejlebedev31838@gmail.com>
2024-07-15 14:21:33 +0700

    Update settings.py

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config.DEBUG
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = [
@@ -164,7 +164,7 @@ USE_TZ = True
 # ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_DIRS = [
     os.path.join(BASE_DIR, 'static')
@@ -181,10 +181,13 @@ STATICFILES_URL = 'staticfiles/'
 STATICFILES_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS_ROOT = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Включаем обслуживание статических файлов
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
