eec414e153852870f6171d3a3125c777b54086e5
Sergey Lebedev <sergejlebedev31838@gmail.com>
2024-08-02 01:41:07 +0700

    Update admin.py

--- advertisements/application/admin.py
+++ advertisements/application/admin.py
@@ -1,6 +1,8 @@
 """ Admin interface for accessing """
 
 from django.contrib import admin
+from django.contrib import messages
+
 from .models import WindowsApplication, AndroidApplication
 
 
@@ -12,12 +14,17 @@ class WindowsApplicationAdmin(admin.ModelAdmin):
     list_display = [
         'id', 'name',
         'show_image_application_main', 'show_image_application_2', 'show_image_application_3',
-        'name_file_main', 'file_main',
+        'name_file_main',
         'created_date',
+        'show_verified',
     ]
 
     list_filter = [
-        'created_at', 'updated_at'
+        'created_at', 'updated_at', 'verified',
+    ]
+
+    actions = [
+        'make_verified_as_true', 'make_verified_as_false',
     ]
 
     fieldsets = (
@@ -25,7 +32,7 @@ class WindowsApplicationAdmin(admin.ModelAdmin):
             'fields': ('user', 'name', 'title',
                        'short_description', 'full_description',
                        'image_application_main',
-                       'name_file_main', 'file_main')
+                       'name_file_main', 'file_main', 'verified',)
         }),
 
         ('Дополнительные медиа данные', {
@@ -36,6 +43,36 @@ class WindowsApplicationAdmin(admin.ModelAdmin):
             'classes': ['collapse']
         })
     )
+
+    @admin.action(description='Файл проверен администратором сайта')
+    def make_verified_as_true(self, request, queryset):
+        """Make the possibility of bargaining"""
+        updated = queryset.update(verified=True)
+
+        if updated:
+            self.message_user(request, f'{updated} Файл проверен администратором сайта.', messages.SUCCESS)
+        else:
+            self.message_user(request, 'Действие не было выполнено.', messages.WARNING)
+
+    @admin.action(description='Файл не проверен администратором сайта')
+    def make_verified_as_false(self, request, queryset):
+        """Remove the possibility of bargaining"""
+        updated = queryset.update(verified=False)
+
+        if updated:
+            self.message_user(request, f'{updated} Файл не проверен администратором сайта.', messages.SUCCESS)
+        else:
+            self.message_user(request, 'Действие не было выполнено.', messages.WARNING)
+
+    # Данный говнокодище нахилдится в разработке...
+    @admin.action(description='Make name application')
+    def make_name_application(self, request, queryset):
+        """Изменяет название выбранных приложений на 'New Application Name'"""
+        updated = queryset.update(name='New Application Name')
+        if updated:
+            self.message_user(request, f'{updated} приложений было переименовано.', messages.SUCCESS)
+        else:
+            self.message_user(request, 'Ни одно приложение не было переименовано.', messages.WARNING)
 
 
 class AndroidApplicationAdmin(admin.ModelAdmin):
@@ -44,12 +81,17 @@ class AndroidApplicationAdmin(admin.ModelAdmin):
     list_display = [
         'id', 'name',
         'show_image_application_main', 'show_image_application_2', 'show_image_application_3',
-        'name_file_main', 'file_main',
+        'name_file_main',
         'created_date',
+        'show_verified',
     ]
 
     list_filter = [
-        'created_at', 'updated_at'
+        'created_at', 'updated_at', 'verified',
+    ]
+
+    actions = [
+        'make_verified_as_true', 'make_verified_as_false',
     ]
 
     fieldsets = (
@@ -57,7 +99,7 @@ class AndroidApplicationAdmin(admin.ModelAdmin):
             'fields': ('user', 'name', 'title',
                        'short_description', 'full_description',
                        'image_application_main',
-                       'name_file_main', 'file_main')
+                       'name_file_main', 'file_main', 'verified',)
         }),
 
         ('Дополнительные медиа данные', {
@@ -68,6 +110,26 @@ class AndroidApplicationAdmin(admin.ModelAdmin):
             'classes': ['collapse']
         })
     )
+
+    @admin.action(description='Файл проверен администратором сайта')
+    def make_verified_as_true(self, request, queryset):
+        """Make the possibility of bargaining"""
+        updated = queryset.update(verified=True)
+
+        if updated:
+            self.message_user(request, f'{updated} Файл проверен администратором сайта.', messages.SUCCESS)
+        else:
+            self.message_user(request, 'Действие не было выполнено.', messages.WARNING)
+
+    @admin.action(description='Файл не проверен администратором сайта')
+    def make_verified_as_false(self, request, queryset):
+        """Remove the possibility of bargaining"""
+        updated = queryset.update(verified=False)
+
+        if updated:
+            self.message_user(request, f'{updated} Файл не проверен администратором сайта.', messages.SUCCESS)
+        else:
+            self.message_user(request, 'Действие не было выполнено.', messages.WARNING)
 
 
 """ Registration methods """
