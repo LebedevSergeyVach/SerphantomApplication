2847cb85d09449d0b44b41475167eea878c2f5aa
Sergey Lebedev <sergejlebedev31838@gmail.com>
2024-08-06 04:46:24 +0700

    Update views.py

--- advertisements/main/views.py
+++ advertisements/main/views.py
@@ -14,12 +14,52 @@ class WebViews(object):
 
     def welcome(request):
         """ Return the welcome """
+        name_site = 'Serphantom'
+        title = 'SERPHANROM SPACE'
+        display = 'Добро пожаловать!'
+        text = 'На данном сайте можете скачать интересующие Вас приложения.'
+
+        button_Windows = 'Windows'
+        button_Android = 'Android'
+        button_GitHub = 'GitHub'
+        button_admin = 'Admin'
+
+        context = {
+            'name_site': name_site,
+            'title': title,
+            'display': display,
+            'text': text,
+            'button_Windows': button_Windows,
+            'button_Android': button_Android,
+            'button_GitHub': button_GitHub,
+            'button_admin': button_admin,
+        }
+
         return render(
-            request, 'welcome.html'
+            request, 'welcome.html', context=context
         )
 
     def not_found(request):
         """ Return the not_found """
+        name_site = 'Страница не найдена'
+        title = '404'
+        display_opps = 'Page not found.'
+        display_not_found = 'Добро пожаловать!'
+        text = 'The page you’re looking for doesn’t exist.'
+
+        button_go_home = 'Go Home'
+        button_admin = 'Admin'
+
+        context = {
+            'name_site': name_site,
+            'title': title,
+            'display_opps': display_opps,
+            'display_not_found': display_not_found,
+            'text': text,
+            'button_go_home': button_go_home,
+            'button_admin': button_admin,
+        }
+
         return render(
-            request, '404.html'
+            request, '404.html', context=context
         )
