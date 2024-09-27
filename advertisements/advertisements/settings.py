7f8712ac20ba0dd1e202a4728ed3ed98f49a10d3
Sergey Lebedev <sergejlebedev31838@gmail.com>
2024-09-27 13:52:54 +0700

    Main: Update settings.py (SSL)

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -222,3 +222,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Включаем обслуживание статических файлов
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+# SSL SERTIFICATES
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
